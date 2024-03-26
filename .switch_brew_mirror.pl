81e7dfa29cd56e6a4607cc05c3e365b261f4cc09
zhaiduting <zhaiduting@icloud.com>
2024-03-27 00:54:43 +0800

    镜像切换脚本代码优化

--- .switch_brew_mirror.pl
+++ .switch_brew_mirror.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 my $CURRENT_MIRROR = "tsinghua";
-my %mirror_urls    = (
+my %MIRROR_URLS    = (
     aliyun => {
         HOMEBREW_API_DOMAIN =>
           "https://mirrors.aliyun.com/homebrew-bottles/api",
@@ -26,14 +26,19 @@ my %mirror_urls    = (
           "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
     }
 );
-my $script_file = $0;    # Current script file
+
+my $SCRIPT_FILE = $0;
 
 sub switch_brew_mirror {
     my $mirror_param = shift;
+
+    # If $mirror_param is empty, set it to $CURRENT_MIRROR
     if ( $mirror_param eq '' ) {
         $mirror_param = $CURRENT_MIRROR;
     }
+
     my $mirror_key = get_mirror_key($mirror_param);
+
     if ( $mirror_key eq 'RESET_MIRROR' ) {
         update_mirror_config('0');
         unset_brew_mirror();
@@ -49,63 +54,52 @@ sub switch_brew_mirror {
 
 sub get_mirror_key {
     my $mirror_param = shift;
-    if ( exists $mirror_urls{$mirror_param} ) {
-        return $mirror_param;
-    }
-    elsif ( $mirror_param =~ /\D/ ) {
-        return 'PARAM_ERROR';
-    }
-    elsif ( $mirror_param == 0 ) {
-        return 'RESET_MIRROR';
-    }
+    return 'RESET_MIRROR' if $mirror_param eq '0';
+    return $mirror_param  if exists $MIRROR_URLS{$mirror_param};
+    return 'PARAM_ERROR'
+      if !defined($mirror_param)
+      || $mirror_param =~
+      /\D/;    # Check if $mirror_param is not defined or not numeric
 
-    my @sorted_keys = sort keys %mirror_urls;
+    my @sorted_keys = sort keys %MIRROR_URLS;
     my $index       = int($mirror_param);
-    if ( $index > 0 && $index <= @sorted_keys ) {
-        return $sorted_keys[ $index - 1 ];
-    }
-    return 'PARAM_ERROR';
+    return ( $index > 0 && $index <= @sorted_keys )
+      ? $sorted_keys[ $index - 1 ]
+      : 'PARAM_ERROR';
 }
 
 sub update_mirror_config {
     my $new_mirror = shift;
     if ( $new_mirror ne $CURRENT_MIRROR ) {
-        local @ARGV = ($script_file);
+        local @ARGV = ($SCRIPT_FILE);
         local $^I   = '';
         while (<>) {
             s/^my \$CURRENT_MIRROR = .*;/my \$CURRENT_MIRROR = "$new_mirror";/;
             print;
         }
-        if ($new_mirror) {
-            print qq{echo "Switched to $new_mirror mirror."\n};
-        }
-        else {
-            print qq{echo "Using Homebrew default URL."\n};
-        }
+        print $new_mirror
+          ? qq{echo "Switched to $new_mirror mirror."\n}
+          : qq{echo "Using Homebrew default URL."\n};
     }
 }
 
 sub export_mirror_to_shell {
     my $mirror_key     = shift;
-    my $mirror_hashref = $mirror_urls{$mirror_key};
-    foreach my $key ( keys %$mirror_hashref ) {
-        my $value = $mirror_hashref->{$key};
-        print qq{export $key="$value"\n};
-    }
+    my $mirror_hashref = $MIRROR_URLS{$mirror_key};
+    print qq{export $_="$mirror_hashref->{$_}"\n} for keys %$mirror_hashref;
 }
 
 sub echo_mirror_usage {
     my $p              = shift;
-    my $mirror_options = join( '|', ( 0, sort keys %mirror_urls ) );
+    my $mirror_options = join( '|', ( 0, sort keys %MIRROR_URLS ) );
     print
 qq{echo "Invalid parameter $p. Usage: switch_brew_mirror [$mirror_options]"\n};
 }
 
-sub unset_brew_mirror() {
-    my $first_mirror_settings = ( values %mirror_urls )[0];
-    for my $key ( keys %$first_mirror_settings ) {
-        print qq{unset "$key"\n};
-    }
+sub unset_brew_mirror {
+    my $first_mirror_settings = ( values %MIRROR_URLS )[0];
+    print qq{unset "$_"\n} for keys %$first_mirror_settings;
 }
+
 switch_brew_mirror( $ARGV[0] );    # Pass the command line argument directly
 
